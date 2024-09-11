1930f2209c3ae641a44d476967ec236c380b1e4a
mauro.abrantes <mauro.henrique07@hotmail.com.br>
2024-09-10 23:40:06 -0300

    second-version

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ function clickbtn() {
 }
 
 async function searchcity(city){
-    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`).then(repost => repost.json())
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`).then(response => response.json())
+    showDados(dados)
     console.log(dados)
+}
+
+function showDados(dados){
+    document.querySelector(".text-city").innerHTML = "Tempo em " +dados.name
+    document.querySelector(".qtd-temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".text-weather").innerHTML = dados.weather[0].description
+    document.querySelector(".humidity").innerHTML = dados.main.humidity + "% de umidade"
+    document.querySelector(".img-weather").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
 }
